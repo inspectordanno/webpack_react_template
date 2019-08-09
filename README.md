77b293b4eacce647ef2ef6e7fc72f28b7e96981b
Dan Spector <danno2423@gmail.com>
2019-08-08 22:30:20 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# webpack_react_template
+ webpack react template
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      loader: 'babel-loader',
+      test: /\.js$/,
+      // options: {
+      //   presets: [
+      //     '@babel/env', '@babel/react'
+      //   ]
+      // },
+      exclude: /node_modules/
+    
+    }, {
+      test: /\.s?css$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'sass-loader'
+      ]  
+    }]
+  },
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    historyApiFallback: true
+  },
+  externals: []
+};
